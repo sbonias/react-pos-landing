d70d7547734b53f484a8bb03ed9fc47767f314d6
Sam Bonias <sbonias84@gmail.com>
2020-08-25 13:50:30 -0400

    Updates

---
+++ precache-manifest.dc98c71293d11aee27b9034dd98ac57a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b5fc6d5c039c4bd98d7c7c866493bd6",
+    "url": "/react-pos-landing/index.html"
+  },
+  {
+    "revision": "c43b62811395491753bd",
+    "url": "/react-pos-landing/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "5d7cab94c9221de1f99e",
+    "url": "/react-pos-landing/static/css/main.21b1dfb3.chunk.css"
+  },
+  {
+    "revision": "c43b62811395491753bd",
+    "url": "/react-pos-landing/static/js/2.bb791048.chunk.js"
+  },
+  {
+    "revision": "33ade7f87243ebea1c83e915f94afce3",
+    "url": "/react-pos-landing/static/js/2.bb791048.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d7cab94c9221de1f99e",
+    "url": "/react-pos-landing/static/js/main.b2eb9036.chunk.js"
+  },
+  {
+    "revision": "d297c4c87cc36096e366",
+    "url": "/react-pos-landing/static/js/runtime-main.0a9e75f4.js"
+  },
+  {
+    "revision": "22e5adc825c7a945a58512b611f34df0",
+    "url": "/react-pos-landing/static/media/foodtruck.22e5adc8.jpg"
+  },
+  {
+    "revision": "f40ed5b1e791e9d2479ba8b5e67f2df8",
+    "url": "/react-pos-landing/static/media/map-image.f40ed5b1.png"
+  }
+]);
